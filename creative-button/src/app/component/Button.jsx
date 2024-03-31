cbaaeaa9901c6a739550fecf86252e44860fd701
MylesGit <37464485+Sayinq@users.noreply.github.com>
2024-03-31 13:31:03 -0400

    Button, fully customizable so far. May implement using a logo instead.

--- creative-button/src/app/component/Button.jsx
+++ creative-button/src/app/component/Button.jsx
@@ -1,42 +1,110 @@
 'use client'
-import React, { useState } from 'react';
+
+import React, { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
+import gsap from 'gsap';
 
-//Component
-import Magnetic from './magnetic';
+const Magnetic = ({ children }) => {
+    const magnetic = useRef(null);
 
-const Button = () => {
+    useEffect(() => {
+        const xTo = gsap.quickTo(magnetic.current, 'x', { duration: 1, ease: 'elastic.out(1, 0.3)' });
+        const yTo = gsap.quickTo(magnetic.current, 'y', { duration: 1, ease: 'elastic.out(1, 0.3)' });
+
+        magnetic.current.addEventListener('mousemove', (e) => {
+            const { clientX, clientY } = e;
+            const { height, width, left, top } = magnetic.current.getBoundingClientRect();
+            const x = clientX - (left + width / 2);
+            const y = clientY - (top + height / 2);
+            xTo(x);
+            yTo(y);
+        });
+
+        magnetic.current.addEventListener('mouseleave', () => {
+            xTo(0);
+            yTo(0);
+        });
+    }, []);
+
+    return React.cloneElement(children, { ref: magnetic });
+};
+
+const TextMagnet = ({ children, distance = 8 }) => {
+    const magnetic = useRef(null);
+
+    useEffect(() => {
+        const xTo = gsap.quickTo(magnetic.current, 'x', { duration: 1, ease: 'elastic.out(1, 0.3)' });
+        const yTo = gsap.quickTo(magnetic.current, 'y', { duration: 1, ease: 'elastic.out(1, 0.3)' });
+
+        magnetic.current.addEventListener('mousemove', (e) => {
+            const { clientX, clientY } = e;
+            const { height, width, left, top } = magnetic.current.getBoundingClientRect();
+            const x = (clientX - (left + width / 2)) / distance;
+            const y = (clientY - (top + height / 2)) / distance;
+            xTo(x);
+            yTo(y);
+        });
+
+        magnetic.current.addEventListener('mouseleave', () => {
+            xTo(0);
+            yTo(0);
+        });
+    }, [distance]);
+
+    return React.cloneElement(children, { ref: magnetic });
+};
+
+const Button = ({ 
+    main, 
+    number,
+    displayOption,
+    bgColor,
+    borderColor,
+    textColor,
+    hoverTextColor,
+    overlayColor,
+}) => {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
-            <Magnetic>
+        <Magnetic>
+            <motion.div
+                style={{ 
+                    border: `1.5px solid ${borderColor}`, 
+                    color: isHovered ? hoverTextColor : textColor,
+                    backgroundColor: `${bgColor}` 
+                }}
+
+                className="relative flex justify-center items-center w-full h-[4.75em] max-w-[200px] text-[1em] border-[1.5px] rounded-[2.125em] p-0 transition-colors duration-500 ease-in-out cursor-pointer px-2 overflow-hidden"
+                onHoverStart={() => setIsHovered(true)}
+                onHoverEnd={() => setIsHovered(false)}
+            >
                 <motion.div
-                    className="relative flex justify-center items-center w-full h-[4.75em] max-w-[200px] text-[1em] hover:text-white text-[#1a1a1a] border-[1.5px] border-[#1a1a1a]/10 rounded-[2.125em] bg-transparent p-0 transition-colors duration-500 ease-in-out cursor-pointer px-2 overflow-hidden"
-                    onHoverStart={() => setIsHovered(true)}
-                    onHoverEnd={() => setIsHovered(false)}
-                >
-                    <motion.div
-                        initial={{ translateY: -100 }}
-                        animate={{ translateY: isHovered ? 0 : -100 }}
-                        end={{ translateY: isHovered ? 100 : 0 }}
-                        transition={{ duration: 0.25, delay: 0.15, ease: 'easeInOut' }}
-                        className="absolute top-0 left-0 w-full h-full bg-[#148CEE] rounded-[2.125em]"
-                    />
-                    <div className="relative w-fit h-fit select-none">
+                    initial={{ y: '-100%' }}
+                    animate={isHovered ? { y: 0 } : { y: '-100%' }}
+                    exit={{ y: '100%' }}
+                    transition={{ duration: 0.25, delay: 0.15, ease: 'easeInOut' }}
+                    style={{ 
+                        backgroundColor: `${overlayColor}`,
+                        display: displayOption === "hidden" ? "none" : "flex",
+                    }}
+                    className="absolute top-0 left-0 w-full h-full rounded-[2.125em]"
+                />
+                <TextMagnet>
+                    <div className="relative flex justify-center items-center w-full h-full select-none">
                         <div className="w-fit h-fit">
-                            <motion.span
-                                className="text-lg font-normal"
-                            >
-                                Development
+                            <motion.span className="text-lg font-normal">
+                                {main}
                             </motion.span>
-                        </div>
-                        <div className="absolute top-[-30%] right-[-15%] w-fit h-fit">
-                            <span className="text-xs text-inherit/30">11</span>
+                            <div className="absolute top-[20%] right-[12%] w-fit h-fit">
+                                <span className="text-xs text-[#9e9e9d]">{number}</span>
+                            </div>
                         </div>
                     </div>
-                </motion.div>
-            </Magnetic>
-        );
-    };
+                </TextMagnet>
+            </motion.div>
+        </Magnetic>
+    );
+};
 
 export default Button;
--- creative-button/src/app/page.js
+++ creative-button/src/app/page.js
@@ -7,7 +7,18 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <section className="flex justify-center items-center w-screen h-screen">
-        <Button />
+        <div className="flex lg:flex-row flex-col justify-center items-center w-full gap-4">
+          <Button 
+            main="Development" // Main text
+            number="11" // Exponent type number
+            displayOption="flex" // Hidden, None, and Flex option, if you want a hover overlay or not
+            bgColor=""
+            borderColor="rgb(26, 26, 26, 0.1)" // RGB value for opacity control 
+            textColor="#1a1a1a" // Base text color
+            hoverTextColor="#F0EFEF" // Color when text is hovered
+            overlayColor="#3048d1"
+          />
+        </div>
       </section>
     </main>
   );
